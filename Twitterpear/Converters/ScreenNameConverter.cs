a24cd4f44c7edfb839618b434744c3b460d2c80e
Colin Kiama <colinkiama@gmail.com>
2019-04-14 16:57:38 +0100

    Twiiter Screen names now shown with '@'

---
+++ Twitterpear/Converters/ScreenNameConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Twitterpear.Converters
+{
+    class ScreenNameConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string valueToReturn = "";
+            if (value is string screenName)
+            {
+                valueToReturn = '@' + screenName;
+            }
+            return valueToReturn;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
